7b22682719c6c0480af96ba025c9d52853cedb32
danielcarv92 <daniel_carvalhinho@hotmail.com>
2017-11-17 16:42:27 +0000

    Criação da classe trilho
    criar classe trilho

---
+++ Trails4Health/Trails4Health/Models/Trilho.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class Trilho
+    {
+        public string TrihoID { get; set; }
+        public string nome { get; set; }
+        public string Inicio { get; set; }
+        public string Fim { get; set; }//nota:procurar variavel cordgps
+        public int AltitudeMax { get; set; }
+        public int AltitudeMin { get; set; }
+        public string descriçao { get; set; }
+        public string IDutilizador { get; set; }
+        public string IDDificuldade { get; set; }
+        public int InteresseHistorico { get; set; }
+        public int BelezaPai { get; set; }
+    }
+}

ea83ca1e2ab9d7515ebe2473764f136575e9af4c
ivopi <ivo.pinto92@gmail.com>
2017-11-17 16:15:53 +0000

    Completar campos da class Image
    declarar o tipo de campos que definem a imagem

--- Trails4Health/Trails4Health/Models/Image.cs
+++ Trails4Health/Trails4Health/Models/Image.cs
@@ -8,5 +8,12 @@ namespace Trails4Health.Models
     public class Image
     {
         public string ImageID { get; set; }
+        public string Localizacao { get; set; } //(FK) id da tabela localizacao
+        public DateTime DataHora { get; set; }
+        public string Id_Utilizador { get; set; } //Utilizador que itroduziu foto
+        public string EstacaoAno { get; set; }
+        public string Tipo { get; set; } //Fauna Flora Histórico
+        public bool Aprovada { get; set; } //Imagem aprovada por administrador
+        public string Url { get; set; } //caminho para a imagem
     }
 }

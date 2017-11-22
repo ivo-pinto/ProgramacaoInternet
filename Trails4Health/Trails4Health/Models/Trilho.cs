9620d39c7c4db06a4f11f00eacc843e4967c609b
ivopi <ivo.pinto92@gmail.com>
2017-11-22 11:20:46 +0000

    clear

--- Trails4Health/Trails4Health/Models/Trilho.cs
+++ Trails4Health/Trails4Health/Models/Trilho.cs
@@ -8,15 +8,27 @@ namespace Trails4Health.Models
     public class Trilho
     {
         public string TrihoID { get; set; }
+<<<<<<< HEAD
         public string Nome { get; set; }
+=======
+        public string nome { get; set; }
+>>>>>>> DanielCarvalhinho(17/11/17)
         public string Inicio { get; set; }
         public string Fim { get; set; }//nota:procurar variavel cordgps
         public int AltitudeMax { get; set; }
         public int AltitudeMin { get; set; }
+<<<<<<< HEAD
         public string Descricao { get; set; }
         public string IDutilizador { get; set; }
         public string IDDificuldade { get; set; }
         public int InteresseHistorico { get; set; }
         public int BelezaPai { get; set; }
+=======
+        public string descriçao { get; set; }
+        public string IDutilizador { get; set; }
+        public string IDDificuldade { get; set; }
+        public int InteresseHistorico { get; set; }
+        public int BelezaPaisagistica { get; set; }
+>>>>>>> DanielCarvalhinho(17/11/17)
     }
 }

bb3066222a8a8615e793463917f9942a9b628fc3
ivopi <ivo.pinto92@gmail.com>
2017-11-23 16:37:23 +0000

    alterações a validação de campos da tabela trilho

--- Trails4Health/Trails4Health/Models/Trilho.cs
+++ Trails4Health/Trails4Health/Models/Trilho.cs
@@ -11,28 +11,28 @@ namespace Trails4Health.Models
 
         public int TrihoId { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
+        [StringLength(maximumLength: 60, MinimumLength = 3)]
         public string Nome { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
+        [StringLength(maximumLength: 60, MinimumLength = 3)]
         public string Inicio { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
+        [StringLength(maximumLength: 60, MinimumLength = 3)]
         public string Fim { get; set; }//nota:procurar variavel cordgps
 
         public int AltitudeMax { get; set; }
         public int AltitudeMin { get; set; }
 
-        [StringLength(1000, MinimumLength = 5)]
+        [StringLength(maximumLength: 1000, MinimumLength = 5)]
         public string Descricao { get; set; }
 
-        [Range(1,5)]
+        [Range(minimum: 1, maximum: 5)]
         public int DificuldadeId { get; set; }
 
-        [Range(1,5)]
+        [Range(minimum: 1, maximum: 5)]
         public int InteresseHistorico { get; set; }
 
-        [Range(1,5)]
+        [Range(minimum: 1, maximum: 5)]
         public int BelezaPai { get; set; }
 
         public ICollection<FotosTrilho> FotosTrilhos { get; set; }

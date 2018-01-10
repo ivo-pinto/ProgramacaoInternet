3d27d3afe936064549683edb8abd292d3d1e4087
ivopi <ivo.pinto92@gmail.com>
2018-01-10 18:10:24 +0000

    clear code

--- Trails4Health/Trails4Health/Startup.cs
+++ Trails4Health/Trails4Health/Startup.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-//using Trails4Health.Data;
-using Microsoft.EntityFrameworkCore;
 using Trails4Health.Models;
 using Trails4Health.Data;
-using Trails4Health.Models;
 using Trails4Health.Services;
 
 namespace Trails4Health

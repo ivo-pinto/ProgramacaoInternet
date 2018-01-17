2877ad2307493782079aa7c04bfd593e6dda722e
ivopi <ivo.pinto92@gmail.com>
2018-01-17 01:08:32 +0000

    Alterações com vista a implementação de roles

--- Trails4Health/Trails4Health/Trails4Health/Data/Trails4HealthUsersDbContext.cs
+++ Trails4Health/Trails4Health/Trails4Health/Data/Trails4HealthUsersDbContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Trails4Health.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Trails4Health.Data
 {
@@ -14,6 +15,23 @@ namespace Trails4Health.Data
             : base(options)
         {
         }
+
+/*  REVER CÓDIGO-----------------------------------------------------------------------------------------------------------------------
+        public static async Task EnsurePopulatedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            // ...
+            if (!await roleManager.RoleExistsAsync("Administrator")) await roleManager.CreateAsync(new IdentityRole("Administrator"));
+            if (!await roleManager.RoleExistsAsync("Customer")) await roleManager.CreateAsync(new IdentityRole("Customer"));
+            // ...
+            ApplicationUser user = await userManager.FindByNameAsync(adminUser);
+            if (user == null)
+            {
+                user = new ApplicationUser() { UserName = adminUser };
+                await userManager.CreateAsync(user, adminPassword);
+            }
+            if (!await userManager.IsInRoleAsync(user, "Administrator")) await userManager.AddToRoleAsync(user, "Administrator");
+        }
+        */
 
         protected override void OnModelCreating(ModelBuilder builder)
         {

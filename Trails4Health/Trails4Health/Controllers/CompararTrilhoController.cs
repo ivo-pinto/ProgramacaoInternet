2adfcecfa9ae07fcb634280d9076dfd2b253d957
danielcarv92 <daniel_carvalhinho@hotmail.com>
2018-01-17 01:52:43 +0000

    controlador, view viewmodel do comparar trilho

---
+++ Trails4Health/Trails4Health/Controllers/CompararTrilhoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Trails4Health.Models;
+using Trails4Health.Models.ViewModels;
+
+namespace Trails4Health.Controllers
+{
+    public class CompararTrilhoController
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CompararTrilhoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trilhos/Details/5
+        public async Task<IActionResult> CompararTrilho(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trilho1 = await _context.Trilhos
+                .SingleOrDefaultAsync(m => m.TrihoId == id);
+
+            var trilho2 = await _context.Trilhos
+               .SingleOrDefaultAsync(m => m.TrihoId == id1);
+
+            if (trilho == null)
+            {
+                return NotFound();
+            }
+
+            return View(trilho);
+        }
+
+
+
+    }
+}
---
+++ Trails4Health/Trails4Health/Models/ViewModels/CompararTrilhoViewModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models.ViewModels
+{
+    public class CompararTrilhoViewModels
+    {
+    }
+}

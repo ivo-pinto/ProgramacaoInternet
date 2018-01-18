ef4f7ed9b848e5006bed81c5bfe72a2b07488f61
ivopi <ivo.pinto92@gmail.com>
2018-01-18 11:04:13 +0000

    Alteraçoes a view Details da Foto

--- Trails4Health/Trails4Health/Models/ViewModels/FotoDetailsViewModel.cs
+++ Trails4Health/Trails4Health/Models/ViewModels/FotoDetailsViewModel.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 namespace Trails4Health.Models.ViewModels
 {
-    public class FotoEditViewModel
+    public class FotoDetailsViewModel
     {
         public Foto Foto { get; set; }
-        public byte[] Imagem { get; set; }
+        public IEnumerable<Trilho> TrilhosFoto { get; set; }
     }
 }
